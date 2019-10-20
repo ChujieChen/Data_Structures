a5f718dd8464df573be4c13b3e6390910bac270e
Chujie Chen <cchen641@gatech.edu>
2019-10-20 10:37:34 -0400

    week4 - 1/6
    Direct addressing - use Array instead of ArrayList

---
+++ src/week3/PhoneBook.java
@@ -0,0 +1,169 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class PhoneBook {
+
+    private FastScanner in = new FastScanner();
+    /**
+	 * Original naive way
+	 */
+    /*
+    // Keep list of all existing (i.e. not deleted yet) contacts.
+    private List<Contact> contacts = new ArrayList<>();
+    */
+	/**
+	 * by LCC
+	 */
+    private static String[] contacts = new String[10000000];
+    
+    
+    public static void main(String[] args) {
+    	new PhoneBook().processQueries();
+        /**
+    	 * by LCC
+    	 */
+        Arrays.fill(contacts, null);
+    }
+
+    private Query readQuery() {
+        String type = in.next();
+        int number = in.nextInt();
+        if (type.equals("add")) {
+            String name = in.next();
+            return new Query(type, name, number);
+        } else {
+            return new Query(type, number);
+        }
+    }
+
+    private void writeResponse(String response) {
+        System.out.println(response);
+    }
+
+
+    private void processQuery(Query query) {
+        if (query.type.equals("add")) {
+        	/**
+        	 * Original naive way
+        	 */
+        	/*
+            // if we already have contact with such number,
+            // we should rewrite contact's name
+            boolean wasFound = false;
+            for (Contact contact : contacts)
+                if (contact.number == query.number) {
+                    contact.name = query.name;
+                    wasFound = true;
+                    break;
+                }
+            // otherwise, just add it
+            if (!wasFound)
+                contacts.add(new Contact(query.name, query.number));
+            */
+        	
+        	/**
+        	 * by LCC
+        	 */
+        	contacts[query.number] = query.name;        	
+        } else if (query.type.equals("del")) {
+        	/**
+        	 * Original naive way
+        	 */
+        	/*
+            for (Iterator<Contact> it = contacts.iterator(); it.hasNext(); )
+                if (it.next().number == query.number) {
+                    it.remove();
+                    break;
+                }
+            */
+        	/**
+        	 * by LCC
+        	 */
+        	contacts[query.number] = null;
+        	
+        } else {
+            String response = "not found";
+            /**
+        	 * Original naive way
+        	 */
+            /*
+            for (Contact contact: contacts)
+                if (contact.number == query.number) {
+                    response = contact.name;
+                    break;
+                }
+            */
+            /**
+        	 * by LCC
+        	 */
+            if(contacts[query.number] != null)response = contacts[query.number];
+            writeResponse(response);
+        }
+    }
+
+    public void processQueries() {
+        int queryCount = in.nextInt();
+        for (int i = 0; i < queryCount; ++i)
+            processQuery(readQuery());
+    }
+
+    static class Contact {
+        String name;
+        int number;
+
+        public Contact(String name, int number) {
+            this.name = name;
+            this.number = number;
+        }
+    }
+
+    static class Query {
+        String type;
+        String name;
+        int number;
+
+        public Query(String type, String name, int number) {
+            this.type = type;
+            this.name = name;
+            this.number = number;
+        }
+
+        public Query(String type, int number) {
+            this.type = type;
+            this.number = number;
+        }
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
